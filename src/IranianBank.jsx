8a851c8f5906d2c1bb60f3a0ddcd6aa3a2ca6304
CyberHuginn <cyberhuginn@gmail.com>
2025-11-16 12:12:47 +0330

    fix bug

--- src/IranianBank.jsx
+++ src/IranianBank.jsx
@@ -1,21 +1,23 @@
 'use client'
 import React from "react";
-import { iconMap } from "./iconMap";
+import {iconMap} from "./iconMap";
 
-const IranianBank = ({ name, size = 20, ...rest }) => {
+const IranianBank = ({name, size = 20, ...rest}) => {
     const Icon = iconMap[name];
+    const src = Icon?.default || Icon;
 
     if (!Icon) {
         console.error(`Icon "${name}" not found.`);
         return null;
     }
 
+
     return (
         <img
-            src={Icon}
+            src={src}
             width={size}
             height={size}
-            style={{ width: `${size}px`, height: `${size}px` }}
+            style={{width: `${size}px`, height: `${size}px`}}
             alt={name}
             {...rest}
         />
